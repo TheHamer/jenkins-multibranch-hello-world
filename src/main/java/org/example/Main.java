c6572718b2a2a7a3969ff31cbbc0b64a25f34398
TheHamer <66170230+TheHamer@users.noreply.github.com>
2024-11-04 13:50:40 +0000

    greeting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            Sysystem.out.println("Hellow world");
+            System.out.println("Hello Jenkins");
         }
     }
 }
